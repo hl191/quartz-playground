730ce997882bcd286e76115ddad7ff7509eade8a
hl191 <laner.hannes@hotmail.com>
2021-07-15 11:26:26 +0200

    Utilize ShedLock

--- src/main/java/at/home/scheduling/spring/SpringSchedulingService.java
+++ src/main/java/at/home/scheduling/spring/SpringSchedulingService.java
@@ -2,6 +2,7 @@ package at.home.scheduling.spring;
 
 import at.home.job.SimpleTaskComponent;
 import at.home.scheduling.fixed.SchedulingService;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ public class SpringSchedulingService {
     private SimpleTaskComponent simpleTaskComponent;
 
     @Scheduled(cron = SchedulingService.SCHEDULE_EVERY_MINUTE)
+    @SchedulerLock(name = "springTask")
     void executeTask() {
         LOGGER.info("Starting task by Spring scheduling");
         simpleTaskComponent.execute();
